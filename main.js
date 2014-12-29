74aa777aa3ac17cefe3aede3e03f692b400f5abd
huihuifan <anarfish@gmail.com>
2014-12-29 15:59:36 -0500

    added checkmark that toggles visibility

--- main.js
+++ main.js
@@ -21,3 +21,14 @@ function hideTooltip(e, tooltip) {
     tooltip.style.display = "none";
 
 }
+
+function toggleVisibility(id) {
+   var el = document.getElementById(id);
+
+   if (el.style.visibility=="visible") {
+          el.style.visibility="hidden";
+     }
+     else {
+          el.style.visibility="visible";
+     }
+ }
